89c599220dc9ad26517ed59cbe89a49e33611c7b
AlexT-3 <alexandriatuell@gmail.com>
2025-08-27 09:51:02 -0600

    Add files via upload

---
+++ burn.py
@@ -0,0 +1,13 @@
+import time
+name = input("What is your name?")
+fav_band = input("What is your favorite band?")
+fav_food = input("What is your favorite food?")
+fav_hobby = input("What is your favorite hobby? (write as an -ing verb)")
+
+print("LOL did your parents actually name you " + name + "!?")
+time.sleep(1)
+print("Also, " + fav_band + " SUCKS, I cant beleive how bad your taste is.")
+time.sleep(1)
+print("And wow I cant beleive you like " + fav_food + ". You have bad taste in music AND food.")
+time.sleep(1)
+print("Maybe if you got a job instead of " + fav_hobby + " all the time you could make your parents proud for once.")
